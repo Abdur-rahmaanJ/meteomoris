7eb99ffe3912f4a61e462a7fb40ec0dffd2595d1
Abdur-RahmaanJ <cryptolabour@gmail.com>
2023-11-11 06:31:33 +0400

    chore: complete basic tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,10 +4,7 @@ import meteomoris
 # integration tests todo
 # this one just a dirty scheme for me
 def test_weekforecast():
-
-
     assert isinstance(meteomoris.get_weekforecast(), list)
-
 
 
 def test_moonphase():
@@ -22,15 +19,12 @@ def test_main_message():
     assert isinstance(meteomoris.get_main_message(), str)
 
 
-
 def test_sunrisemu():
     assert isinstance(meteomoris.get_sunrisemu(), dict)
 
 
-
 def test_sunriserodr():
     assert isinstance(meteomoris.get_sunriserodr(), dict)
-
 
 
 def test_eclipses():
@@ -40,6 +34,16 @@ def test_eclipses():
 def test_equinoxes():
     assert isinstance(meteomoris.get_equinoxes(), list)
 
-
 def test_solstices():
     assert isinstance(meteomoris.get_solstices(), list)
+
+def test_tides():
+    assert isinstance(meteomoris.get_tides(), dict)
+
+def test_latest():
+    assert isinstance(meteomoris.get_latest(), dict)
+
+
+def test_uvindex():
+    assert isinstance(meteomoris.get_uvindex(), dict)
+
