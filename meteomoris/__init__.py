e4d13a91abe02d6bb9d84f601eea4e352215f19d
Abdur-RahmaanJ <cryptolabour@gmail.com>
2023-12-24 09:31:59 +0400

    fix: Add remaining get_today_ apis to init

--- meteomoris/__init__.py
+++ meteomoris/__init__.py
@@ -18,9 +18,13 @@ get_solstices = Meteo.get_solstices
 get_tides = Meteo.get_tides
 get_latest = Meteo.get_latest
 get_uvindex = Meteo.get_uvindex
+
+get_today_forecast = Meteo.get_today_forecast
+get_today_sunrise = Meteo.get_today_sunrise
 get_today_eclipse = Meteo.get_today_eclipse
 get_today_moonphase = Meteo.get_today_moonphase
 get_today_solstice = Meteo.get_today_solstice
 get_today_equinox = Meteo.get_today_equinox
+get_today_tides = Meteo.get_today_tides
 
 __version__ = "2.12.0"
