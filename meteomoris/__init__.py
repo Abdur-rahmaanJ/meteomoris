8a52485d94955a643f78f5f45ecd68165ae84abf
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2025-02-27 13:06:44 +0400

    Update __init__.py

--- meteomoris/__init__.py
+++ meteomoris/__init__.py
@@ -26,4 +26,4 @@ get_today_solstice = Meteo.get_today_solstice
 get_today_equinox = Meteo.get_today_equinox
 get_today_tides = Meteo.get_today_tides
 
-__version__ = "2.12.2"
+__version__ = "2.12.3"
